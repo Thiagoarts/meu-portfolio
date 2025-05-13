fc89ef286117a44aa40e586f26166b93bd986c20
Thiago Silva <thiago.sbenedito@icloud.com>
2025-05-13 15:28:30 -0300

    Portfolio Thiago Silva

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/*==============================Typing animation =====================*/
+var typed = new Typed(".typing", {
+    strings:["", "Desenvolvedor Web", "Designer Web", "Programador Mobile", "Developer C#"],
+    typeSpeed:100, 
+    BackSpeed:60,
+    loop:true
+})
